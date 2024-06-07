2d95c5dcbab2a5c6bd55c8fa6e64606897dc4194
Rodrigo Cabral <rodrigocabral1302@gmail.com>
2024-06-07 19:12:33 -0300

    subindo atividade dom 2

---
+++ script.js
@@ -0,0 +1,8 @@
+function Atividade() {
+
+    var numeroum = parseFloat(document.getElementById("numeroum").value)
+    var numerodois = parseFloat(document.getElementById("numerodois").value)
+    
+    document.getElementById("resultado").value = (numeroum + numerodois)
+    
+}
